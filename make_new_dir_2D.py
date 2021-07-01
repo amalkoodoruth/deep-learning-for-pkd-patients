2063629cf50a08860046c8b85803fa4da3d57168
Amal <koodorutham9@gmail.com>
2021-07-01 12:18:21 +0400

    Added script that copies dcm and seg to new dir

---
+++ make_new_dir_2D.py
@@ -0,0 +1,24 @@
+import shutil
+import os
+
+master_path = os.getcwd()
+master_dir = master_path + '/MR/'
+
+for patient in os.listdir(master_dir):
+    if patient != '.DS_Store':
+        dcm_dir = master_dir + patient + '/T2SPIR/DICOM_anon/'
+        seg_dir = master_dir + patient + '/T2SPIR/Ground/'
+
+        for dcm in os.listdir(dcm_dir):
+            if dcm != '.DS_Store':
+                dcm_path = dcm_dir + dcm
+                os.makedirs
+                new_dcm_path = master_path + '/MR2D/Train/X/' + patient + '-' + dcm
+                shutil.copyfile(dcm_path,new_dcm_path)
+
+        for seg in os.listdir(seg_dir):
+            if seg != '.DS_Store':
+                seg_path = seg_dir + seg
+                new_seg_path = master_path + '/MR2D/Train/Y/' + patient + '-' + seg
+                shutil.copyfile(seg_path, new_seg_path)
+
